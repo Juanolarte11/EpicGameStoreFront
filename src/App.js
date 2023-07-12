a623f3638ccce3dd835915c87d9936445b8805a0
Alex Ruiz <alexruiz-1999@hotmail.com>
2023-07-12 16:57:13 -0300

    dashboard

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import About from "./components/about/About.jsx";
 import Favorites from "./components/Favorites/Favorites.jsx";
 import "./App.css";
 import Cart from "./components/Cart/Cart.jsx";
+
 axios.defaults.baseURL = `http://localhost:3001/`;
 
 function App() {
@@ -22,6 +23,7 @@ function App() {
           <Route path="/about" component={About} />
           <Route path="/cart" component={Cart} />
           <Route path="/favorites" component={Favorites} />
+          {/* <Route path="/Dashboard" component={Dashboard}/> */}
         </Switch>
       </div>
     </BrowserRouter>
