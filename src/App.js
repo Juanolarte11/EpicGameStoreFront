61bb91fd301c055e3740b40b0a2bb8469f3ad4e1
Gilberto castaño <gilberto.castano@outlook.es>
2023-07-15 02:04:03 -0500

    ignore

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import LandingPage from "./components/landingPage/LandingPage.jsx";
 import Home from "./components/Home/Home.jsx";
 import CreateVideogame from "./components/createVideogame/CreateVideogame.jsx";
 import Detail from "./components/detail/Detail.jsx";
-import About from "./components/About/About.jsx";
+import About from "./components/about/About.jsx";
 import Favorites from "./components/Favorites/Favorites.jsx";
 import "./App.css";
 import Cart from "./components/Cart/Cart.jsx";
