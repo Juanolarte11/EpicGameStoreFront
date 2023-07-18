b368b42441a23ab237e7c409c3493e44032534b2
ynketh <yankomoreno1@gmail.com>
2023-07-18 03:35:57 -0500

    1231

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -82,11 +82,11 @@ export default function NavBar({ size }) {
         </div>
       )}
         </div>
-        {/* {dataUser?.userID && (
+        {dataUser?.userID && (
             <button onClick={btnClick} className={style.navButton}>
               Logout
             </button>
-          )} */}
+          )}
       </div>
     </nav>
   );
